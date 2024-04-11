333122be3318c5a298f0464b8f1fef053b06b6bb
legogoUA <olehmedushivskyi@gmail.com>
2024-04-11 19:00:21 +0300

    add button

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+var colorWhite = const Color.fromARGB(255, 255, 255, 255);
+var colorBlack = Color.fromARGB(255, 0, 0, 0);
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
@@ -7,17 +10,24 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'LearningByDoing',
+        title: const Text(
+          'LearningByPractice',
           style: TextStyle(
             color: Colors.black,
             fontSize: 18,
             fontWeight: FontWeight.bold
           ),
         ),
-        backgroundColor: Color.fromARGB(0, 0, 0, 0),
+        backgroundColor: colorWhite,
         elevation: 0.0,
         centerTitle: true,
+        leading: Container(
+          margin: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: colorBlack,
+            borderRadius: BorderRadius.circular(10)
+          ),
+        ),
       ),
     );
   }
